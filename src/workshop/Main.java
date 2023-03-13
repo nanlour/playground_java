8607624a02f7dad7de3837456588a072fff269b9
wr133 <tjuwrr@gmail.com>
2023-03-13 16:38:14 +1100

    reformat file structure

---
+++ src/workshop/Main.java
@@ -0,0 +1,12 @@
+/*just a playground
+
+
+ */
+
+public class Main{
+    public static void main(String[] args){
+        workshop_week2 test = new workshop_week2();
+        test.problem1(9);
+        test.problem2(100);
+    }
+}
